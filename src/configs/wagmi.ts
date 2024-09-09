2e2df8bde2af86be8297329ea7f69e39e4c6b80d
Weil Liao <liaoweil05621@gmail.com>
2024-09-09 23:10:52 +0800

    test: check netlify env mode

--- src/configs/wagmi.ts
+++ src/configs/wagmi.ts
@@ -5,6 +5,10 @@ import { injected } from "wagmi/connectors";
 // has setup whitelist
 const INFURA_KEY = import.meta.env.VITE_INFURA_KEY;
 
+console.log(import.meta.env.DEV);
+console.log(import.meta.env.MODE);
+console.log(import.meta.env.PROD);
+
 const mainnetHttp = INFURA_KEY
   ? http(`https://mainnet.infura.io/v3/${INFURA_KEY}`)
   : http();
